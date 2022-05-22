7ab18a98526a8a42527f2f318b42d970f26eb70a
bibletoon <alex8.ivanov@gmail.com>
2022-05-22 22:30:41 +0300

    chore: add sample command and query

---
+++ Sonar.Player/Sonar.Player.Application/PlayerContext/Commands/SampleCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace Sonar.Player.Application.PlayerContext.Commands;
+
+public static class SampleCommand
+{
+    public record Command() : IRequest<Response>;
+
+    public record Response();
+
+    public class CommandHandler : IRequestHandler<Command, Response>
+    {
+        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        {
+        }
+    }
+}
