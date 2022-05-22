6d084a31b8c7e7f175532c2de03a697ba87de0c7
bibletoon <alex8.ivanov@gmail.com>
2022-05-22 21:47:53 +0300

    Feat: add db context

---
+++ Sonar.Player/Sonar.Player.DataAccess/PlayerDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Sonar.Player.Data;
+
+public class PlayerDbContext : DbContext
+{
+    public PlayerDbContext(DbContextOptions<PlayerDbContext> options)
+        : base(options)
+    {
+    }
+}
