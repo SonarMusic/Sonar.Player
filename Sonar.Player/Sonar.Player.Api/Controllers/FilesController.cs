1bc1b04dd4a9b0b50c142bd5418e6b8e9e9679e9
bibletoon <alex8.ivanov@gmail.com>
2022-05-22 21:42:19 +0300

    Feat: add controllers

---
+++ Sonar.Player/Sonar.Player.Api/Controllers/FilesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sonar.Player.Api.Controllers;
+
+[ApiController]
+[Route("files")]
+public class FilesController
+{
+    [HttpPost("/track")]
+    public async Task<IActionResult> UploadTrackAsync([FromQuery] string name)
+    {
+        //TODO: get file from request content
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("/trackStreamInfo")]
+    public async Task<IActionResult> GetTrackStreamInfoAsync([FromQuery(Name = "id")] string trackId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpGet("/{streamPartName}")]
+    public async Task<IActionResult> GetStreamPartAsync(string streamPartName)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("/track}")]
+    public async Task<IActionResult> DeleteTrackAsync([FromQuery] string trackId)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ Sonar.Player/Sonar.Player.Api/Controllers/PlayerContextController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Sonar.Player.Api.Controllers;
+
+public class PlayerContextController
+{
+    [HttpGet("/queue")]
+    public async Task<IActionResult> GetQueueAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("/queue/track")]
+    public async Task<IActionResult> AddTrackToQueueAsync([FromQuery] string trackId)
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpDelete("/queue")]
+    public async Task<IActionResult> PurgeQueueAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    [HttpPatch("/queue/shuffle")]
+    public async Task<IActionResult> ShuffleQueueAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
