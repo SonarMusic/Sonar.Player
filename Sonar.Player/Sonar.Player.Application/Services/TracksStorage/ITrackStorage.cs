da8121573a2c5e89071cf7a9e59777cc1cd83e62
bibletoon <alex8.ivanov@gmail.com>
2022-05-26 12:14:09 +0300

    Feat: add track storage

---
+++ Sonar.Player/Sonar.Player.Application/Services/TracksStorage/ITrackStorage.cs
@@ -0,0 +1,9 @@
+﻿using Sonar.Player.Domain.Entities;
+using Sonar.Player.Domain.Enumerations;
+
+namespace Sonar.Player.Application.Services.TracksStorage;
+
+public interface ITrackStorage
+{
+    Task<Track> SaveTrack(Guid id, AudioFormat format, Stream content);
+}
