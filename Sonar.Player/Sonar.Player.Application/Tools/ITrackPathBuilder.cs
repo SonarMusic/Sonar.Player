2600cc9ab1e75f8180e17a66854135d23ac3486d
bibletoon <alex8.ivanov@gmail.com>
2022-05-26 12:12:54 +0300

    Feat: add path builder

---
+++ Sonar.Player/Sonar.Player.Application/Tools/ITrackPathBuilder.cs
@@ -0,0 +1,8 @@
+﻿namespace Sonar.Player.Application.Tools;
+
+public interface ITrackPathBuilder
+{
+    string GetTrackFolderPath(Guid trackId);
+    string GetTrackStreamInfoPath(Guid trackId);
+    string GetTackStreamPartPath(Guid trackId, string partName);
+}
---
+++ Sonar.Player/Sonar.Player.Application/Tools/TrackPathBuilder.cs
@@ -0,0 +1,20 @@
+﻿namespace Sonar.Player.Application.Tools;
+
+public class TrackPathBuilder : ITrackPathBuilder
+{
+    public string GetTrackFolderPath(Guid trackId)
+    {
+        //TODO: move base directory configuration to appsettings
+        return Path.Combine(Directory.GetCurrentDirectory(), "tracks", trackId.ToString());
+    }
+
+    public string GetTrackStreamInfoPath(Guid trackId)
+    {
+        return Path.Combine(GetTrackFolderPath(trackId), "stream", "streamInfo.m3u8");
+    }
+
+    public string GetTackStreamPartPath(Guid trackId, string partName)
+    {
+        return Path.Combine(GetTrackFolderPath(trackId), "stream", partName);
+    }
+}
