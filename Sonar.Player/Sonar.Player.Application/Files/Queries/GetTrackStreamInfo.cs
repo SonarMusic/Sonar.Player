e3e2a4aa9c4523fcd071587b3e2fefc18111d662
Maksim Polukhin <mdpoluhin@gmail.com>
2022-05-28 16:54:37 +0300

    feat: implement missing queries

--- Sonar.Player/Sonar.Player.Application/Files/Queries/GetTrackStreamInfo.cs
+++ Sonar.Player/Sonar.Player.Application/Files/Queries/GetTrackStreamInfo.cs
@@ -1,18 +1,41 @@
 ﻿using MediatR;
+using Sonar.Player.Application.Tools;
+using Sonar.Player.Application.Tools.Exceptions;
+using Sonar.UserTracksManagement.ApiClient;
 
 namespace Sonar.Player.Application.Files.Queries;
 
 public static class GetTrackStreamInfo
 {
-    public record Query() : IRequest<Response>;
+    public record Query(string Token, Guid TrackId) : IRequest<Response>;
 
     public record Response(FileStream TrackInfoStream);
 
     public class QueryHandler : IRequestHandler<Query, Response>
     {
+        private readonly ITrackPathBuilder _builder;
+        private readonly IUserTracksApiClient _apiClient;
+
+        public QueryHandler(ITrackPathBuilder builder, IUserTracksApiClient apiClient)
+        {
+            _builder = builder;
+            _apiClient = apiClient;
+        }
+
         public async Task<Response> Handle(Query request, CancellationToken cancellationToken)
         {
-            throw new NotImplementedException();
+            bool isEnoughAccess = await _apiClient.IsEnoughAccessAsync(
+                request.Token,
+                request.TrackId,
+                cancellationToken);
+
+            if (!isEnoughAccess)
+            {
+                throw new NotEnoughAccessException($"Not enough access to track {request.TrackId}");
+            }
+
+            var path = _builder.GetTrackStreamInfoPath(request.TrackId);
+            return new Response(File.OpenRead(path));
         }
     }
 }
