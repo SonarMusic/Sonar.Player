375f2af5ca197385cc55e09f298dde670531be29
bibletoon <alex8.ivanov@gmail.com>
2022-05-28 10:45:46 +0300

    Feat: implement delete command

--- Sonar.Player/Sonar.Player.Application/Files/Commands/DeleteTrack.cs
+++ Sonar.Player/Sonar.Player.Application/Files/Commands/DeleteTrack.cs
@@ -1,18 +1,38 @@
 ﻿using MediatR;
+using Sonar.Player.Application.Services.TracksStorage;
+using Sonar.Player.Application.Tools.Exceptions;
+using Sonar.Player.Domain.Models;
+using Sonar.UserTracksManagement.ApiClient;
 
 namespace Sonar.Player.Application.Files.Commands;
 
 public static class DeleteTrack
 {
-    public record Command() : IRequest<Response>;
+    public record Command(User User, Guid TrackId) : IRequest<Unit>;
 
-    public record Response();
-
-    public class CommandHandler : IRequestHandler<Command, Response>
+    public class CommandHandler : IRequestHandler<Command, Unit>
     {
-        public async Task<Response> Handle(Command request, CancellationToken cancellationToken)
+        private readonly IUserTracksApiClient _userTracksApiClient;
+        private readonly ITrackStorage _trackStorage;
+
+        public CommandHandler(IUserTracksApiClient userTracksApiClient, ITrackStorage trackStorage)
         {
-            throw new NotImplementedException();
+            _userTracksApiClient = userTracksApiClient;
+            _trackStorage = trackStorage;
+        }
+
+        public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+        {
+           var isEnoughAccess = await _userTracksApiClient.IsEnoughAccessAsync(
+                                                                           request.User.Token,
+                                                                           request.User.Id,
+                                                                           cancellationToken);
+           if (!isEnoughAccess)
+               throw new NotEnoughAccessException($"Not enough access to track {request.TrackId}");
+
+           //TODO: call _userTracksApiClient.DeleteAsync
+           await _trackStorage.DeleteTrack(request.TrackId);
+           return Unit.Value;
         }
     }
 }
---
+++ Sonar.Player/Sonar.Player.Application/Tools/Exceptions/NotEnoughAccessException.cs
@@ -0,0 +1,7 @@
+﻿namespace Sonar.Player.Application.Tools.Exceptions;
+
+public class NotEnoughAccessException : Exception
+{
+    public NotEnoughAccessException() { }
+    public NotEnoughAccessException(string message) : base(message) { }
+}
