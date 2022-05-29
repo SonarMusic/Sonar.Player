a79c4fd82510723b9187ecf612cf3154affdb58b
Maksim Polukhin <mdpoluhin@gmail.com>
2022-05-29 22:24:43 +0300

    refactor: add swagger bootstrap

--- Sonar.Player/Sonar.Player.Api/Program.cs
+++ Sonar.Player/Sonar.Player.Api/Program.cs
@@ -1,8 +1,7 @@
-using System.Reflection;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 using Serilog;
+using Sonar.Player.Api.Bootstrappers;
 using Sonar.Player.Application.Services;
 using Sonar.Player.Application.Services.TracksStorage;
 using Sonar.Player.Application.Tools;
@@ -15,49 +14,28 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Host.UseSerilog((ctx, lc) => lc.WriteTo.Console());
 
-// Add services to the container.
 builder.Services.AddControllers();
 builder.Services.Configure<RouteOptions>(opt => opt.LowercaseUrls = true);
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen(
-    options =>
-    {
-        options.CustomSchemaIds(type => type.FullName);
-        options.SwaggerDoc(
-            "v1", 
-            new OpenApiInfo(){Title = "Sonar.Player", Version = "v1"});
-        
-        options.AddSecurityDefinition("Token", new OpenApiSecurityScheme
-        {
-            In = ParameterLocation.Header,
-            Description = "Token to access resources",
-            Name = "Authorization",
-            Type = SecuritySchemeType.ApiKey,
-        });
 
-        var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-        options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
-    }
-);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerWithCustomAuthorization();
+
 builder.Services.AddDbContext<PlayerDbContext>(opt => opt.UseSqlite("Filename=player.db"));
+
 builder.Services.AddMediatR(typeof(Sonar.Player.Application.IAssemblyMarker));
+
 builder.Services.AddScoped<IUserService, FakeUserService>();
 builder.Services.AddScoped<IUserTracksApiClient, FakeUserTracksClient>();
 
 builder.Services.AddScoped<ITrackStorage, LocalTrackStorage>();
 builder.Services.Decorate<ITrackStorage, HlsTrackProcessor>();
-
 builder.Services.AddSingleton<ITrackPathBuilder, TrackPathBuilder>();
-
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerWithUI();
 }
 
 app.UseHttpsRedirection();
@@ -66,6 +44,7 @@ app.UseAuthorization();
 
 app.MapControllers();
 app.UseCors(opt => opt.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 app.UseSerilogRequestLogging();
 
 app.Run();
