9363d480de1f3a60c4f3c6c07c2982b22cda0311
DenChika <eholocation@gmail.com>
2022-06-01 19:53:42 +0300

    Feat: implemented queue methods

--- Sonar.Player/Sonar.Player.Domain/Entities/TracksQueue.cs
+++ Sonar.Player/Sonar.Player.Domain/Entities/TracksQueue.cs
@@ -1,33 +1,49 @@
-﻿namespace Sonar.Player.Domain.Entities;
+﻿using Sonar.Player.Domain.Tools.Exceptions;
+
+namespace Sonar.Player.Domain.Entities;
 
 public class TracksQueue
 {
     private readonly List<Track> _tracks = new List<Track>();
 
+    public Track? Current = null;
     public IReadOnlyCollection<Track> Tracks => _tracks.AsReadOnly();
 
     public Track Next()
     {
-        throw new NotImplementedException();
+        if (Current is null)
+            throw new SonarPlayerException("Cannot get the next track: queue is empty");
+        
+        var next = Tracks.SkipWhile(x => x.Id != Current.Id).Skip(1).DefaultIfEmpty(null).First();
+        Current = next ?? throw new SonarPlayerException("Cannot get the next track: this is the last track in queue");
+        return Current;
     }
 
     public void Enqueue(Track track)
     {
-        throw new NotImplementedException();
+        _tracks.Add(track);
     }
 
     public Track Previous()
     {
-        throw new NotImplementedException();
+        if (Current is null)
+            throw new SonarPlayerException("Cannot get the last track: queue is empty");
+
+        var prev = Tracks.TakeWhile(x => x.Id != Current.Id).DefaultIfEmpty(null).Last();
+        Current = prev ?? throw new SonarPlayerException("Cannot get the last track: this is the last track in queue");
+        return Current;
     }
 
     public void Shuffle()
     {
-        throw new NotImplementedException();
+        var rand = new Random();
+        var newTracks = _tracks.OrderBy(x => rand.Next()).ToList();
+        for (var i = 0; i < _tracks.Count; i++)
+            _tracks[i] = newTracks[i];
     }
 
     public void Purge()
     {
-        throw new NotImplementedException();
+        _tracks.Clear();
     }
 }
