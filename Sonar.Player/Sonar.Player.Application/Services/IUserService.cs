0ae947babc49fabd9e59f97dccd4b9516ffc6f4b
bibletoon <alex8.ivanov@gmail.com>
2022-06-25 09:33:37 +0300

    Fix: user service

--- Sonar.Player/Sonar.Player.Application/Services/IUserService.cs
+++ Sonar.Player/Sonar.Player.Application/Services/IUserService.cs
@@ -4,5 +4,5 @@ namespace Sonar.Player.Application.Services;
 
 public interface IUserService
 {
-    Task<User> GetUserAsync(string token);
+    Task<User> GetUserAsync(string token, CancellationToken cancellationToken);
 }
