edb2b9f37d23e76f98a031abf1f21bb66ee6b99e
DenChika <eholocation@gmail.com>
2022-06-26 21:56:39 +0500

    Refactor: delete empty strings

--- Sonar.Player/Sonar.Player.Domain/Enumerations/AudioFormat.cs
+++ Sonar.Player/Sonar.Player.Domain/Enumerations/AudioFormat.cs
@@ -11,21 +11,13 @@ public class AudioFormat : MediaFormat
         : base(name, format) { }
     
     public static AudioFormat Mp3 => new AudioFormat("Mp3", "mp3");
-    
     public static AudioFormat Wav => new AudioFormat("Wav", "wav");
-
     public static AudioFormat Aac => new AudioFormat("Aac", "aac");
-    
     public static AudioFormat Ogg => new AudioFormat("Ogg", "ogg");
-    
     public static AudioFormat Aa => new AudioFormat("Aa", "aa");
-
     public static AudioFormat Aiff => new AudioFormat("Aiff", "aiff");
-
     public static AudioFormat Ac3 => new AudioFormat("Ac3", "ac3");
-    
     public static AudioFormat Adx => new AudioFormat("Adx", "adx");
-    
     public static AudioFormat Wma => new AudioFormat("Wma", "wma");
 
     public static AudioFormat FromFileName(string filename)
